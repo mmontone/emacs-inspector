bc762c9359a78a1634c706648e562e67642b91c7
Mariano Montone <marianomontone@gmail.com>
2022-09-10 18:16:57 -0300

    tree-inspector: usage

--- tree-inspector.el
+++ tree-inspector.el
@@ -25,6 +25,9 @@
 
 ;; Inspector tool for Emacs Lisp object that uses a treeview.
 
+;; Usage:
+;; M-x `tree-inspector-inspect-last-sexp' to inspect the last sexp at point.
+
 ;;; Code:
 
 (require 'eieio)
