8969375c0c9528e916b6d03f64a1661d9a57ca90
Mariano Montone <marianomontone@gmail.com>
2023-02-06 20:31:43 -0300

    More fixes

--- inspector-test.sh
+++ inspector-test.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env sh -e
 emacs --batch -l ert -l inspector.el -l inspector-tests.el -f ert-run-tests-batch-and-exit
-#!/usr/bin/env sh -e
-emacs --batch -l ert -l inspector.el -l inspector-tests.el -f ert-run-tests-batch-and-exit
