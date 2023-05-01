c328475aa433a39dd08702ff7daf5941a3a3efde
Mariano Montone <marianomontone@gmail.com>
2023-05-01 10:53:18 -0300

    Release 0.29

--- inspector.el
+++ inspector.el
@@ -5,7 +5,7 @@
 ;; Author: Mariano Montone <marianomontone@gmail.com>
 ;; URL: https://github.com/mmontone/emacs-inspector
 ;; Keywords: debugging, tool, lisp, development
-;; Version: 0.28
+;; Version: 0.29
 ;; Package-Requires: ((emacs "27.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
